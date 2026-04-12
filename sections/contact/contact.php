ad9f0e2e9887924272cb8fd69d8a8dc64dd966d8
Maksim-Manojlovic <mr.maksim.manojlovic@gmail.com>
2026-04-12 11:09:17 +0200

    [ADD] Added email and phone on contact to be globaly

--- sections/contact/contact.php
+++ sections/contact/contact.php
@@ -3,6 +3,9 @@
  * Section: Kontakt stranica
  * Reuse: get_template_part('sections/contact-page/contact-page')
  */
+
+$telefon = bastovan_get_contact( 'telefon' );
+$email   = bastovan_get_contact( 'email' );
 ?>
 
 <section class="contact section-lg" id="kontakt">
@@ -13,10 +16,22 @@
       <h1 class="heading-xl">Pošaljite upit za<br>uređenje dvorišta</h1>
       <p class="text-lead contact__lead">
         Odgovaramo u roku od 24–48h.<br>
-        Opišite dvorište i dobićete okvirnu ponudu.<br>
-        Email adresa:<br>
-        Broj telefona:
+        Opišite dvorište i dobićete okvirnu ponudu.
       </p>
+      <div class="contact__info">
+        <?php if ( $email ) : ?>
+        <a href="mailto:<?php echo esc_attr( $email ); ?>" class="contact__info-item">
+          <span class="contact__info-label">Email</span>
+          <span class="contact__info-value"><?php echo esc_html( $email ); ?></span>
+        </a>
+        <?php endif; ?>
+        <?php if ( $telefon ) : ?>
+        <a href="tel:<?php echo esc_attr( preg_replace( '/\s/', '', $telefon ) ); ?>" class="contact__info-item">
+          <span class="contact__info-label">Telefon</span>
+          <span class="contact__info-value"><?php echo esc_html( $telefon ); ?></span>
+        </a>
+        <?php endif; ?>
+      </div>
     </div>
 
     <div class="contact__form-wrap">
